1928f3a66a4ca30582d93e5df1465fdabbb0599c
HigorDev8 <higordev8@gmail.com>
2024-05-14 11:47:45 -0300

    Categoria

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,30 @@
-
-import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  let navigate = useNavigate();
-
-  const { usuario, handleLogout } = useContext(AuthContext);
-
-  function logout() {
-    handleLogout();
-    alert("Usuário deslogado com sucesso");
-    navigate("/login");
-  }
-
-  let navbarComponent;
-
-  if (usuario.token !== "") {
-    navbarComponent = (
+  return (
+    <>
       <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
         <div className="container flex justify-between text-lg">
-          <Link to="/home" className="text-2xl font-bold uppercase">
-            Blog Pessoal
+          <Link to="/" className="text-2xl font-bold uppercase">
+            Farmacia
           </Link>
 
           <div className="flex gap-4">
-            <Link to="/postagens" className="hover:underline">
-              Postagens
+            <Link to="/" className="hover:underline">
+              Inicio
             </Link>
-            <Link to="/temas" className="hover:underline">
-              Temas
+            <Link to="/categorias" className="hover:underline">
+              Categorias
             </Link>
-            <Link to="/cadastroTema" className="hover:underline">
-              Cadastrar tema
-            </Link>
-            <Link to="/perfil" className="hover:underline">
-              Perfil
-            </Link>
-            <Link to="" onClick={logout} className="hover:underline">
-              Sair
+            <Link to="/cadastroCategoria" className="hover:underline">
+              Cadastrar categoria
             </Link>
           </div>
         </div>
       </div>
-    );
-  }
-
-  return <>{navbarComponent}</>;
+    </>
+  );
 }
 
 export default Navbar;
