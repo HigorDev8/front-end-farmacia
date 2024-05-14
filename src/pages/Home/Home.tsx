c2b9b6f5d4dd7ba2e920a43ecd934f393e461329
HigorDev8 <higordev8@gmail.com>
2024-05-14 11:50:58 -0300

    Categorias

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,7 @@
+
 import React from "react";
 import homeLogo from "../../assets/home.png";
-import "./Home.css";
-import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens";
-import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -11,22 +10,18 @@ function Home() {
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja bem vinde!</h2>
-            <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+            <p className="text-xl">Farmacia</p>
 
             <div className="flex justify-around gap-4">
-              <ModalPostagem />
-              <button className="rounded bg-white text-blue-800 py-2 px-4">
-                Ver postagens
-              </button>
+              <Link to="/categorias" className="hover:underline">
+                <button className="rounded bg-white text-blue-800 py-2 px-4">
+                  Ver categorias
+                </button>
+              </Link>
             </div>
-          </div>
-
-          <div className="flex justify-center ">
-            <img src={homeLogo} alt="" className="w-2/3" />
           </div>
         </div>
       </div>
-      <ListaPostagens />
     </>
   );
 }
